a6698833a198df37e38946655e4b34f7d408d2ff
Ury <117782130+Ury0@users.noreply.github.com>
2024-11-16 18:36:07 -0300

    semi pronto

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import express from 'express';
 import routes from './routes.js';
 import db from './src/db.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config(); // Certifique-se de carregar as variáveis de ambiente
 
 const app = express();
+
+const corsOptions = { 
+    origin: 'http://localhost:4200', // Permitir apenas esta origem 
+    optionsSuccessStatus: 200 // Alguns navegadores exigem esse status para solicitações bem-sucedidas 
+}; 
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use('/api', routes); // Use as rotas com um prefixo, se preferir
